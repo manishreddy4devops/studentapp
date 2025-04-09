923c6a246e6c477c067e74d96f1417fb101e3aa3
root <root@ICPL35618.infostretch.com>
2025-04-09 12:24:59 +0000

    after adding colouring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css';
 import StudentList from './components/StudentList';
 import AddStudent from './components/AddStudent';
 import ManageStudent from './components/ManageStudent';
